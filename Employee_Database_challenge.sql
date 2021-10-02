a9691e0a23e79f225434b70c88d3cba4abbbf4d5
Guliz des Tombe <gulizdestombe@macbook-air.lan>
2021-10-02 12:03:01 -0700

    final submission

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -1,3 +1,4 @@
+--Create a table where employees eligible to retire are identified and titles are specified
 select employees.emp_no, 
 	employees.first_name,
 	employees.last_name,
@@ -12,12 +13,15 @@ on employees.emp_no=titles.emp_no
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 order by employees.emp_no;
 
--- Use Dictinct with Orderby to remove duplicate rows
+
+-- Remove duplicates from the retirement_table and create a new table with this clean data
 SELECT DISTINCT ON (emp_no) emp_no,last_name,first_name, title
 
 INTO unique_titles
 FROM retirement_titles
 ORDER BY emp_no, to_date desc;
+
+--Obtain the number of emplyees eligible to retire and group by title
 
 select count(unique_titles.title) as "number of titles", title
 into Retiring_titles
@@ -25,7 +29,24 @@ from unique_titles
 group by title
 order by "number of titles" desc
 
+--Identify current employees that are eligible for the mentorship program
+select emp.emp_no,
+emp.first_name,
+emp.last_name,
+emp.birth_date,
+de.from_date,
+de.to_date,
+ts.title
+into Mentorship_Eligibility
+from employees as emp
+inner join dept_emp as de
+on(emp.emp_no=de.emp_no)
+inner join titles as ts
+on(emp.emp_no=ts.emp_no)
+where de.to_date = ('9999-01-01') and emp.birth_date between '1965-01-01'and '1965-12-31'
 
-select* from unique_titles
+--This was a fun challenge.  Thank you.  Onward!
 
-drop table retiring_tables
+
+
+
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Pewlett-Hackard-Analysis
+
+## Overview of Project
+
+### Purpose
+
+1. Determine how many employees are eligible to retire per job title.
+
+2. Identify all employees that eligible for the mentorship program based on the given criteria.
+
+## Results
+
+1.  We noted that here is a large number of senior engineers and senior staff eligible for retirement (over 5000, combined).  Wehereas only 2 emplyees management are eligible to retire.
+	
+
+2.  We identified 2383 employees eligible for the mentorship program.  Staff, senior staff, engineers, and senior engineers are all around 500 whereas assistant managers and technical leaders are fewer, about 78.  No managers were identified as eligible for mentorship.
+
+### Summary
+
+Based on this data, the number of employees eligible for mentorship across skills and job titles is sufficient to counter the potential loss of knowledge and expertise of senior engineers and skilled employees.
